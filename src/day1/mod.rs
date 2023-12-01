f445bb30567e8f0c209d3b461b2dd6301bedac7d
Austin Almond <aust.almond@gmail.com>
2023-12-01 07:24:21 -0500

    File IO
    from https://doc.rust-lang.org/rust-by-example/std_misc/file/open.html

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,5 +1,26 @@
 // https://adventofcode.com/2023/day/1
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 pub fn run() {
     println!("Day 1");
+
+    // Create a path to the desired file
+    let path = Path::new("src/day1/input.txt");
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains:\n{}", display, s),
+    }
 }
