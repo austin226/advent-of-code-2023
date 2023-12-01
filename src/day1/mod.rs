0d2a3f7585ed2e5cc0af5fb841a027ce915934df
Austin Almond <aust.almond@gmail.com>
2023-12-01 09:24:48 -0500

    support digits as words

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -42,25 +42,32 @@ where
 
 /// Concatenate the first and last digit characters in the string together.
 fn secret_number(line: &String) -> u32 {
-    let regex = Regex::new(r"(?:(\d).*(\d).*|(\d).*)").expect("vaild regex");
-    for caps in regex.captures_iter(line) {
-        if caps.len() != 4 {
-            panic!("line '{line}' has {} captures", caps.len());
+    let re = Regex::new(r"\d|one|two|three|four|five|six|seven|eight|nine").expect("vaild regex");
+    let matches: Vec<_> = re.find_iter(line).map(|m| m.as_str()).collect();
+    let first = parse_word_digit(matches[0]);
+    let last = parse_word_digit(matches[matches.len() - 1]);
+    first * 10 + last
+}
+
+fn parse_word_digit(word: &str) -> u32 {
+    if let Ok(d) = word.parse::<u32>() {
+        // word is already a digit
+        if (1..=9).contains(&d) {
+            return d;
         }
-
-        let (first, last) = if let (Some(first), Some(last)) = (caps.get(1), caps.get(2)) {
-            (first, last)
-        } else if let Some(only) = caps.get(3) {
-            (only, only)
-        } else {
-            panic!("Invalid line - {line}");
-        };
-
-        let tens = first.as_str().parse::<u32>().unwrap() * 10;
-        let ones = last.as_str().parse::<u32>().unwrap();
-        return tens + ones;
     }
-    panic!("No captures in line {line}");
+    match word {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        _ => panic!("Invalid word digit: {word}"),
+    }
 }
 
 #[cfg(test)]
@@ -70,7 +77,7 @@ mod tests {
     #[test]
     fn test_secret_number() {
         for (line, exp_res) in [
-            ("1abc2", 12),
+            ("1abc2two", 12),
             ("pqr3stu8vwx", 38),
             ("a1b2c3d4e5f", 15),
             ("treb7uchet", 77),
@@ -81,8 +88,17 @@ mod tests {
             ("aa1aa2", 12),
             ("aa12aa", 12),
             ("12345", 15),
+            ("one2three", 13),
+            ("1two", 12),
+            ("1four7", 17),
+            ("asixa", 66),
         ] {
             assert_eq!(exp_res, secret_number(&line.to_string()), "line={line}");
         }
+    }
+
+    #[test]
+    fn parse_word() {
+        assert_eq!(parse_word_digit("one"), 1);
     }
 }
