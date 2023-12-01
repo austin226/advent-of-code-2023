93b6f95db5f3218a38086c525cc9653a31be259c
Austin Almond <aust.almond@gmail.com>
2023-12-01 08:58:51 -0500

    implement secret_number

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,5 +1,6 @@
 // https://adventofcode.com/2023/day/1
 
+use regex::Regex;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
@@ -13,8 +14,8 @@ pub fn run() {
             for line in lines {
                 match line {
                     Ok(line) => {
-                        let line_sum = line_sum(&line);
-                        println!("{} / {}", line_sum, line);
+                        let secret_number = secret_number(&line);
+                        println!("{} / {}", secret_number, line);
                     }
                     Err(err) => {
                         panic!("Cannot read line in {} - {}", path.display(), err);
@@ -38,10 +39,49 @@ where
     Ok(std::io::BufReader::new(file).lines())
 }
 
-/// Add the first and last digit characters in the string together.
-fn line_sum(line: &String) -> u32 {
-    // TODO
-    let regex = r"(?:(\d).*(\d)[^\n$]*(?:\n|$)|(\d).*(?:\n|$))";
-    println!("{}", regex);
-    0
+/// Concatenate the first and last digit characters in the string together.
+fn secret_number(line: &String) -> u32 {
+    let regex = Regex::new(r"(?:(\d).*(\d)[^\n$]*(?:\n|$)|(\d).*(?:\n|$))").expect("vaild regex");
+    for caps in regex.captures_iter(line) {
+        if caps.len() != 4 {
+            panic!("line '{line}' has {} captures", caps.len());
+        }
+
+        let (first, last) = if let (Some(first), Some(last)) = (caps.get(1), caps.get(2)) {
+            (first, last)
+        } else if let Some(only) = caps.get(3) {
+            (only, only)
+        } else {
+            panic!("Invalid line - {line}");
+        };
+
+        let tens = first.as_str().parse::<u32>().unwrap() * 10;
+        let ones = last.as_str().parse::<u32>().unwrap();
+        return tens + ones;
+    }
+    panic!("No captures in line {line}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_secret_number() {
+        for (line, exp_res) in [
+            ("1abc2", 12),
+            ("pqr3stu8vwx", 38),
+            ("a1b2c3d4e5f", 15),
+            ("treb7uchet", 77),
+            ("12aaaaaa", 12),
+            ("1aaaa2aaa", 12),
+            ("aaa1aa2aaa", 12),
+            ("1aa2", 12),
+            ("aa1aa2", 12),
+            ("aa12aa", 12),
+            ("12345", 15),
+        ] {
+            assert_eq!(exp_res, secret_number(&line.to_string()), "line={line}");
+        }
+    }
 }
