977b60283d040be0195668cbdfe255db3941a31c
Austin Almond <aust.almond@gmail.com>
2023-12-02 00:28:29 -0500

    Solution for day 2

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,15 +1,80 @@
 // https://adventofcode.com/2023/day/2
 
-use regex::Regex;
-use std::collections::HashSet;
-use std::fs::File;
-use std::io::prelude::*;
 use std::path::Path;
 
 use crate::common::read_lines;
 
+const N_RED: u32 = 12;
+const N_GREEN: u32 = 13;
+const N_BLUE: u32 = 14;
+
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    turns: Vec<Turn>,
+}
+
+impl Game {
+    fn new(id: u32) -> Self {
+        Self {
+            id,
+            turns: Vec::new(),
+        }
+    }
+
+    fn add_turn(&mut self, turn: Turn) {
+        self.turns.push(turn);
+    }
+
+    fn is_possible(&self) -> bool {
+        let (red, green, blue) = self.turns.iter().fold((0, 0, 0), |sum, turn| {
+            (sum.0 + turn.red, sum.1 + turn.green, sum.2 + turn.blue)
+        });
+        red <= N_RED && green <= N_GREEN && blue <= N_BLUE
+    }
+}
+
+#[derive(Debug)]
+struct Turn {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+impl Turn {
+    fn parse(summary: &str) -> Self {
+        let colors_str: Vec<&str> = summary.split(", ").collect();
+        println!("colors: {:?}", colors_str);
+
+        let mut red = 0;
+        let mut green = 0;
+        let mut blue = 0;
+
+        for color_str in colors_str.iter() {
+            let c: Vec<&str> = color_str.split(" ").collect();
+            let n: u32 = c[0].to_string().parse().unwrap();
+            let color = c[1];
+            match color {
+                "red" => {
+                    red += n;
+                }
+                "green" => {
+                    green += n;
+                }
+                "blue" => {
+                    blue += n;
+                }
+                _ => {
+                    panic!("unknown color {}", color);
+                }
+            };
+        }
+
+        Self { red, green, blue }
+    }
+}
+
 pub fn run() {
-    // Create a path to the desired file
     let path = Path::new("src/day2/input0.txt");
     let mut sum = 0;
     match read_lines(path) {
@@ -17,7 +82,23 @@ pub fn run() {
             for line in lines {
                 match line {
                     Ok(line) => {
-                        // TODO
+                        let s: Vec<&str> = line.split(": ").collect();
+
+                        let game_and_id: Vec<&str> = s[0].split(" ").collect();
+                        let game_id: u32 = game_and_id[1].to_string().parse().unwrap();
+
+                        let mut game = Game::new(game_id);
+
+                        let turn_summaries: Vec<&str> = s[1].split("; ").collect();
+                        for turn_summary in turn_summaries.iter() {
+                            let turn = Turn::parse(&turn_summary);
+                            game.add_turn(turn);
+                        }
+
+                        if game.is_possible() {
+                            println!("Game {:?} is possible", game);
+                            sum += game.id;
+                        }
                     }
                     Err(err) => {
                         panic!("Cannot read line in {} - {}", path.display(), err);
@@ -30,5 +111,6 @@ pub fn run() {
         }
     }
 
-    println!("{}", sum);
+    // Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+    println!("sum: {}", sum);
 }
