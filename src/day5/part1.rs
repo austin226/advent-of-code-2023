97ea2b4eb2e8de3b583a782f65bc462811c0dd6c
Austin Almond <aust.almond@gmail.com>
2023-12-05 00:00:25 -0500

    day5 start

---
+++ src/day5/part1.rs
@@ -0,0 +1,14 @@
+use std::collections::HashSet;
+
+use crate::common::get_input;
+
+pub fn run() {
+    let input = get_input("src/day5/input0.txt");
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {}
+}
