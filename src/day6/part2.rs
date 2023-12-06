290dd0821c27179fac19c42f6aae6961ee7b5d55
Austin Almond <aust.almond@gmail.com>
2023-12-06 15:04:42 -0500

    solve day 6 part 2

--- src/day6/part2.rs
+++ src/day6/part2.rs
@@ -5,37 +5,19 @@ use crate::common::get_input;
 pub fn run() {
     let input = get_input("src/day6/input1.txt");
 
-    let times = {
-        let times_str = input[0].split_ascii_whitespace().collect_vec();
-        &times_str[1..times_str.len()]
-            .iter()
-            .map(|s| s.parse::<i32>().unwrap())
-            .collect_vec()
-    };
-    let distances = {
-        let dist_str = input[1].split_ascii_whitespace().collect_vec();
-        &dist_str[1..dist_str.len()]
-            .iter()
-            .map(|s| s.parse::<i32>().unwrap())
-            .collect_vec()
-    };
+    let race_time = input[0].split(":").collect_vec()[1]
+        .replace(" ", "")
+        .parse::<i32>()
+        .unwrap();
+    let record_dist = input[1].split(":").collect_vec()[1]
+        .replace(" ", "")
+        .parse::<i64>()
+        .unwrap();
 
-    debug_assert_eq!(times.len(), distances.len());
+    let ways_to_win = (1..race_time)
+        .map(|charge_time| (race_time - charge_time) as i64 * charge_time as i64)
+        .filter(|&dist| dist > record_dist)
+        .count();
 
-    let mut product = 1;
-    for i in 0..times.len() {
-        let race_time = times[i];
-        let record_dist = distances[i];
-
-        let ways_to_win = (1..race_time)
-            .map(|charge_time| (race_time - charge_time) as i64 * charge_time as i64)
-            .filter(|&dist| dist > record_dist as i64)
-            .count();
-
-        product *= ways_to_win;
-        println!("{ways_to_win} ways to win against time {race_time}");
-    }
-
-    println!("{:?}, {:?}", times, distances);
-    println!("Product: {product}");
+    println!("Ways to win: {ways_to_win}");
 }
