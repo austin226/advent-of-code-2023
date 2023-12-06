a7fd39ee98c79b03d3b6b52684db9ee5972997e7
Austin Almond <aust.almond@gmail.com>
2023-12-06 14:58:40 -0500

    copy part 1 to part 2

--- src/day6/part2.rs
+++ src/day6/part2.rs
@@ -1,5 +1,41 @@
+use itertools::Itertools;
+
 use crate::common::get_input;
 
 pub fn run() {
-    let input = get_input("src/day6/input0.txt");
+    let input = get_input("src/day6/input1.txt");
+
+    let times = {
+        let times_str = input[0].split_ascii_whitespace().collect_vec();
+        &times_str[1..times_str.len()]
+            .iter()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect_vec()
+    };
+    let distances = {
+        let dist_str = input[1].split_ascii_whitespace().collect_vec();
+        &dist_str[1..dist_str.len()]
+            .iter()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect_vec()
+    };
+
+    debug_assert_eq!(times.len(), distances.len());
+
+    let mut product = 1;
+    for i in 0..times.len() {
+        let race_time = times[i];
+        let record_dist = distances[i];
+
+        let ways_to_win = (1..race_time)
+            .map(|charge_time| (race_time - charge_time) as i64 * charge_time as i64)
+            .filter(|&dist| dist > record_dist as i64)
+            .count();
+
+        product *= ways_to_win;
+        println!("{ways_to_win} ways to win against time {race_time}");
+    }
+
+    println!("{:?}, {:?}", times, distances);
+    println!("Product: {product}");
 }
