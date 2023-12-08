e71dcaddcb73ab4b9089de88667c9b86f02ee380
Austin Almond <aust.almond@gmail.com>
2023-12-08 08:13:53 -0500

    read lines

--- src/day8/part1.rs
+++ src/day8/part1.rs
@@ -1,5 +1,18 @@
 use crate::common::get_input;
 
+struct Node {
+    name: String,
+    left: String,
+    right: String,
+}
+
 pub fn run() {
     let input = get_input("src/day8/input0.txt");
+
+    let left_right_instructions = input[0].chars();
+
+    for line in &input[2..] {
+        
+        println!("{:?}", line);
+    }
 }
