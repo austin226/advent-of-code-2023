7bcd7acd2d97e8974a623033187a8c8a3d97fcd4
Austin Almond <aust.almond@gmail.com>
2023-12-08 09:40:10 -0500

    solve d8p2 on test input

--- src/day8/part2.rs
+++ src/day8/part2.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use crate::common::get_input;
+use itertools::Itertools;
 use regex::Regex;
 
 #[derive(Debug)]
@@ -35,23 +36,34 @@ impl Direction {
 }
 
 pub fn run() {
-    let input = get_input("src/day8/input0.txt");
+    let input = get_input("src/day8/input3.txt");
 
     let directions = parse_directions(&input[0]);
     let nodes = parse_nodes(&input);
+    println!("{:?}", nodes);
 
-    // Start at node AAA
-    let mut current_node = "AAA".to_string();
+    // Start at all nodes ending in A
+    let mut current_nodes = nodes
+        .keys()
+        .filter(|n| n.ends_with("A"))
+        .map(|n| n.clone())
+        .collect_vec();
     let mut dir_idx = 0;
     let mut n_steps = 0;
-    // println!("Started at {current_node}");
-    while current_node != "ZZZ" {
+    println!("Started at {:?}", current_nodes);
+
+    // While not all current nodes end in z
+    while !current_nodes.iter().all(|n| n.ends_with("Z")) {
         // Count each step.
         n_steps += 1;
 
         let direction = &directions[dir_idx];
-        current_node = nodes[&current_node].next(direction);
-        // println!("{:?} to {current_node}", direction);
+
+        current_nodes = current_nodes
+            .iter()
+            .map(|n| nodes[n].next(direction))
+            .collect_vec();
+        println!("{:?} to {:?}", direction, current_nodes);
 
         // Go to next direction, or wrap back around.
         dir_idx = if dir_idx == directions.len() - 1 {
@@ -71,7 +83,7 @@ fn parse_directions(line: &String) -> Vec<Direction> {
 fn parse_nodes(input: &Vec<String>) -> HashMap<String, Node> {
     let mut nodes = HashMap::new();
     for line in &input[2..] {
-        let re = Regex::new(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)").unwrap();
+        let re = Regex::new(r"([A-Z0-9]{3}) = \(([A-Z0-9]{3}), ([A-Z0-9]{3})\)").unwrap();
         for (_, [n, l, r]) in re.captures_iter(line).map(|caps| caps.extract()) {
             let name = n.to_string();
             let left = l.to_string();
