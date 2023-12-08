0d8d9a84a68a0a22353f3e9112b05323713743d4
Austin Almond <aust.almond@gmail.com>
2023-12-08 08:30:52 -0500

    parse nodes using regex

--- src/day8/part1.rs
+++ src/day8/part1.rs
@@ -1,5 +1,7 @@
 use crate::common::get_input;
+use regex::Regex;
 
+#[derive(Debug)]
 struct Node {
     name: String,
     left: String,
@@ -11,8 +13,17 @@ pub fn run() {
 
     let left_right_instructions = input[0].chars();
 
+    let mut nodes = Vec::<Node>::new();
     for line in &input[2..] {
-        
-        println!("{:?}", line);
+        let re = Regex::new(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)").unwrap();
+        for (_, [n, l, r]) in re.captures_iter(line).map(|caps| caps.extract()) {
+            nodes.push(Node {
+                name: n.to_string(),
+                left: l.to_string(),
+                right: r.to_string(),
+            });
+        }
     }
+
+    println!("{:?}", nodes);
 }
