0df2c80fc626f14f4293d09c4c9d2501a6f00249
Austin Almond <aust.almond@gmail.com>
2023-12-08 08:35:37 -0500

    parse directions and nodes

--- src/day8/part1.rs
+++ src/day8/part1.rs
@@ -1,4 +1,5 @@
 use crate::common::get_input;
+use itertools::Itertools;
 use regex::Regex;
 
 #[derive(Debug)]
@@ -8,11 +9,37 @@ struct Node {
     right: String,
 }
 
+#[derive(Debug)]
+enum Direction {
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn parse(c: char) -> Self {
+        match c {
+            'R' => Self::Right,
+            'L' => Self::Left,
+            _ => panic!("Invalid direction: {c}"),
+        }
+    }
+}
+
 pub fn run() {
     let input = get_input("src/day8/input0.txt");
 
-    let left_right_instructions = input[0].chars();
+    let directions = parse_directions(&input[0]);
+    let nodes = parse_nodes(&input);
 
+    println!("{:?}", directions);
+    println!("{:?}", nodes);
+}
+
+fn parse_directions(line: &String) -> Vec<Direction> {
+    line.chars().map(|c| Direction::parse(c)).collect()
+}
+
+fn parse_nodes(input: &Vec<String>) -> Vec<Node> {
     let mut nodes = Vec::<Node>::new();
     for line in &input[2..] {
         let re = Regex::new(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)").unwrap();
@@ -24,6 +51,5 @@ pub fn run() {
             });
         }
     }
-
-    println!("{:?}", nodes);
+    return nodes;
 }
