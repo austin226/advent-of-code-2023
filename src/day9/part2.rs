ab812275de40f30bafb9c762768df4b4434af466
Austin Almond <aust.almond@gmail.com>
2023-12-09 15:03:11 -0500

    d9p2, super easy lol

--- src/day9/part2.rs
+++ src/day9/part2.rs
@@ -17,11 +17,11 @@ fn extrapolate(vals: &Vec<i64>) -> i64 {
     let slopes = (0..vals.len() - 1)
         .map(|i| vals[i + 1] - vals[i])
         .collect_vec();
-    vals.last().unwrap() + extrapolate(&slopes)
+    vals.first().unwrap() - extrapolate(&slopes)
 }
 
 pub fn run() {
-    let input = get_input("src/day9/input0.txt");
+    let input = get_input("src/day9/input1.txt");
 
     // values range from -100,000,000 to 100,000,000
     let result: i64 = input
