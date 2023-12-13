5e7029f899ebceed9319b41f7a62d5fe93655395
Austin Almond <aust.almond@gmail.com>
2023-12-13 07:30:19 -0500

    start day 13

---
+++ src/day13/mod.rs
@@ -0,0 +1,8 @@
+// https://adventofcode.com/2023/day/X
+
+mod part1;
+mod part2;
+
+pub fn run() {
+    part1::run();
+}
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod day1;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 mod day2;
 mod day3;
 mod day4;
@@ -15,5 +16,5 @@ mod day8;
 mod day9;
 
 fn main() {
-    day12::run();
+    day13::run();
 }
