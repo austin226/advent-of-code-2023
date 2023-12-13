b7b53d85d4bc229732b24140353ece5eced3ce54
Austin Almond <aust.almond@gmail.com>
2023-12-13 07:38:43 -0500

    break input into patterns

--- src/day13/part1.rs
+++ src/day13/part1.rs
@@ -2,4 +2,19 @@ use crate::common::get_input;
 
 pub fn run() {
     let input = get_input("src/day13/input0.txt");
+
+    let mut patterns = Vec::new();
+    let mut n_spaces = 0;
+    for line in input {
+        if patterns.is_empty() || n_spaces > (patterns.len() - 1) {
+            patterns.push(Vec::new());
+        }
+        if line == "" {
+            n_spaces += 1;
+        } else {
+            patterns[n_spaces].push(line);
+        }
+    }
+
+    println!("{:?}", patterns);
 }
