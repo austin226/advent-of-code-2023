f5d8d4b923ab30e6bed5aa1d597ea6ef7a6b2ae6
Austin Almond <aust.almond@gmail.com>
2023-12-13 07:43:13 -0500

    set up answer

--- src/day13/part1.rs
+++ src/day13/part1.rs
@@ -1,5 +1,15 @@
 use crate::common::get_input;
 
+fn reflect_left_cols(pattern: &Vec<String>) -> Option<u32> {
+    // TODO
+    None
+}
+
+fn reflect_up_rows(pattern: &Vec<String>) -> Option<u32> {
+    // TODO
+    None
+}
+
 pub fn run() {
     let input = get_input("src/day13/input0.txt");
 
@@ -16,5 +26,16 @@ pub fn run() {
         }
     }
 
-    println!("{:?}", patterns);
+    let ans = patterns
+        .iter()
+        .map(
+            |pattern| match (reflect_left_cols(pattern), reflect_up_rows(pattern)) {
+                (Some(c), None) => c,
+                (None, Some(r)) => 100 * r,
+                _ => panic!("No symmetry for pattern: {:?}", pattern),
+            },
+        )
+        .sum::<u32>();
+
+    println!("{:?}", ans);
 }
