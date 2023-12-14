40774d319a83ce54f2de5e83878f6f6ce3d685b7
Austin Almond <aust.almond@gmail.com>
2023-12-14 07:46:29 -0500

    start d14

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod day10;
 mod day11;
 mod day12;
 mod day13;
+mod day14;
 mod day2;
 mod day3;
 mod day4;
@@ -16,5 +17,5 @@ mod day8;
 mod day9;
 
 fn main() {
-    day12::run();
+    day14::run();
 }
