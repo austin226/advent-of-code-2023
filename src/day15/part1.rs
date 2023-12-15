ad1636a75f64932321b086623a5f71dcadf49321
Austin Almond <aust.almond@gmail.com>
2023-12-15 08:16:39 -0500

    d15p1

--- src/day15/part1.rs
+++ src/day15/part1.rs
@@ -1,5 +1,14 @@
 use crate::common::get_input;
 
+fn hash(step_input: &str) -> u32 {
+    step_input
+        .bytes()
+        .fold(0, |acc, x| ((((acc as u32) + (x as u32)) * 17) % 256))
+}
+
 pub fn run() {
-    let input = get_input("src/day15/input0.txt");
+    let input = get_input("src/day15/input1.txt");
+    let input_steps = input[0].split(',');
+    let ans = input_steps.fold(0u32, |acc, x| acc + hash(x));
+    println!("{ans}");
 }
