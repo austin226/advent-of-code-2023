85e776eb3697f49328ef1f5df8f406c7886df917
Austin Almond <aust.almond@gmail.com>
2023-12-14 22:23:47 -0500

    set up d15

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod day11;
 mod day12;
 mod day13;
 mod day14;
+mod day15;
 mod day2;
 mod day3;
 mod day4;
@@ -17,5 +18,5 @@ mod day8;
 mod day9;
 
 fn main() {
-    day14::run();
+    day15::run();
 }
