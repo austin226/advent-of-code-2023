083126f57fa618ee114cd7d28d5fa63f654410f9
Austin Almond <aust.almond@gmail.com>
2023-12-17 08:42:21 -0500

    use struct for graph

--- src/day17/part1.rs
+++ src/day17/part1.rs
@@ -2,13 +2,28 @@ use itertools::Itertools;
 
 use crate::common::get_input;
 
+#[derive(Debug)]
+struct Graph {
+    size: usize,
+    matrix: Vec<Vec<u8>>,
+}
+
+impl Graph {
+    fn new(input: &Vec<String>) -> Self
+    {
+        let size = input.len();
+        let matrix: Vec<Vec<u8>> = input.iter().map(|row| {
+            assert_eq!(size, row.len(), "Matrix must be square");
+            row.chars().map(|c| {
+                c.to_digit(10).expect("Parsing a digit") as u8
+            }).collect_vec()
+        }).collect_vec();
+        Self { size, matrix }
+    }
+}
+
 pub fn run() {
     let input = get_input("src/day17/input0.txt");
-    let matrix: Vec<Vec<u8>> = input.iter().map(|row| {
-        row.chars().map(|c| {
-            c.to_digit(10).expect("Parsing a digit") as u8
-        }).collect_vec()
-    }).collect_vec();
-
-    println!("{:?}", matrix);
+    let graph = Graph::new(&input);
+    println!("{:?}", graph);
 }
