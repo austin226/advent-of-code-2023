2f4431678dfaeabeee1fc83fe686cb7ffd5bf7f5
Austin Almond <aust.almond@gmail.com>
2023-12-17 08:39:15 -0500

    parse matrix

--- src/day17/part1.rs
+++ src/day17/part1.rs
@@ -1,5 +1,14 @@
+use itertools::Itertools;
+
 use crate::common::get_input;
 
 pub fn run() {
     let input = get_input("src/day17/input0.txt");
+    let matrix: Vec<Vec<u8>> = input.iter().map(|row| {
+        row.chars().map(|c| {
+            c.to_digit(10).expect("Parsing a digit") as u8
+        }).collect_vec()
+    }).collect_vec();
+
+    println!("{:?}", matrix);
 }
