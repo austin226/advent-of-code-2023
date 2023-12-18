901fa816ad9368cd8f26f9878f098953ec595832
Austin Almond <aust.almond@gmail.com>
2023-12-18 06:46:25 -0500

    set up data structures

--- src/day18/part1.rs
+++ src/day18/part1.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+
 use crate::common::get_input;
 
 enum Direction {
@@ -15,7 +16,7 @@ impl Direction {
             "R" => Direction::R,
             "D" => Direction::D,
             "L" => Direction::L,
-            _ => panic!("Bad direction {input}")
+            _ => panic!("Bad direction {input}"),
         }
     }
 }
@@ -32,6 +33,26 @@ impl Color {
     }
 }
 
+struct Coord {
+    x: i32,
+    y: i32,
+}
+
+struct Tile {
+    coord: Coord,
+    color: Color,
+}
+
+struct Map {
+    tiles: Vec<Vec<Tile>>,
+}
+
+impl Map {
+    fn new() -> Self {
+        Self { tiles: Vec::new() }
+    }
+}
+
 struct Step {
     direction: Direction,
     distance: i32,
@@ -39,15 +60,39 @@ struct Step {
 }
 
 impl Step {
-    fn new(input: String) -> Self {
+    fn new(input: &str) -> Self {
         let tokens = input.split_ascii_whitespace().collect_vec();
         let direction = Direction::new(tokens[0]);
         let distance = tokens[1].parse::<i32>().unwrap();
         let color = Color::new(tokens[2]);
-        Self { direction, distance, color }
+        Self {
+            direction,
+            distance,
+            color,
+        }
     }
+}
+
+struct Worker {
+    location: Coord,
+}
+
+impl Worker {
+    fn new() -> Self {
+        Self {
+            location: Coord { x: 0, y: 0 },
+        }
+    }
+
+    fn perform_step(&self, step: &Step, map: &mut Map) {}
 }
 
 pub fn run() {
     let input = get_input("src/day18/input0.txt");
+
+    let worker = Worker::new();
+    let map = Map::new();
+    let steps = input.iter().map(|line| line.as_str()).map(Step::new);
+
+    for step in steps {}
 }
