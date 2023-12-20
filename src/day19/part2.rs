e17908f76e22627686141083cbf2d78dd4db541c
Austin Almond <aust.almond@gmail.com>
2023-12-19 19:55:57 -0500

    Remove Parts

--- src/day19/part2.rs
+++ src/day19/part2.rs
@@ -27,56 +27,6 @@ impl Attribute {
 }
 
 #[derive(Debug)]
-struct Part {
-    x: i32,
-    m: i32,
-    a: i32,
-    s: i32,
-}
-
-impl Part {
-    fn parse(line: &str) -> Option<Self> {
-        static PART_REGEX: Lazy<Regex> =
-            Lazy::new(|| Regex::new(r"\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}").unwrap());
-        let re = &PART_REGEX;
-        let caps = re.captures(line)?;
-        let x = caps.get(1)?.as_str().parse::<i32>().ok()?;
-        let m = caps.get(2)?.as_str().parse::<i32>().ok()?;
-        let a = caps.get(3)?.as_str().parse::<i32>().ok()?;
-        let s = caps.get(4)?.as_str().parse::<i32>().ok()?;
-
-        Some(Self { x, m, a, s })
-    }
-
-    fn get_attribute(&self, attribute: &Attribute) -> i32 {
-        match attribute {
-            Attribute::X => self.x,
-            Attribute::M => self.m,
-            Attribute::A => self.a,
-            Attribute::S => self.s,
-        }
-    }
-
-    fn rating(&self) -> i32 {
-        self.x + self.m + self.a + self.s
-    }
-
-    fn meets_condition(&self, condition: &Condition) -> bool {
-        match condition {
-            Condition::Always => true,
-            Condition::AttrGreaterThan {
-                attribute,
-                threshold,
-            } => self.get_attribute(attribute) > *threshold,
-            Condition::AttrLessThan {
-                attribute,
-                threshold,
-            } => self.get_attribute(attribute) < *threshold,
-        }
-    }
-}
-
-#[derive(Debug)]
 enum Condition {
     /// Condition always passes
     Always,
@@ -242,66 +192,25 @@ impl Workflow {
 
 struct System {
     workflows: HashMap<String, Workflow>,
-    parts: Vec<Part>,
 }
 
 impl System {
     fn parse(input: &Vec<String>) -> Self {
         let mut workflows = HashMap::new();
-        let mut parts = Vec::new();
 
         for line in input {
             if let Some(workflow) = Workflow::parse(line.as_str()) {
                 workflows.insert(workflow.name.clone(), workflow);
-            } else if let Some(part) = Part::parse(line.as_str()) {
-                parts.push(part);
             }
         }
 
-        Self { workflows, parts }
-    }
-
-    /// Return the sum of all accepted parts.
-    fn process_parts(&self) -> i32 {
-        self.parts
-            .par_iter()
-            // .iter()
-            .filter(|part| self.process_part(part))
-            .map(|part| part.rating())
-            .sum()
-    }
-
-    /// Return whether the part is accepted.
-    fn process_part(&self, part: &Part) -> bool {
-        // println!("Process part {:?}", part);
-        let mut current_workflow_name = "in".to_string();
-
-        loop {
-            // println!("Workflow {current_workflow_name}");
-            let workflow = self
-                .workflows
-                .get(&current_workflow_name)
-                .unwrap_or_else(|| panic!("workflow {current_workflow_name} not found"));
-            for rule in workflow.rules.iter() {
-                // println!("Checking rule {:?}", rule);
-                if part.meets_condition(&rule.condition) {
-                    match rule.destination.clone() {
-                        Destination::Terminal { accept: true } => return true,
-                        Destination::Terminal { accept: false } => return false,
-                        Destination::Next { workflow_name } => {
-                            current_workflow_name = workflow_name;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
+        Self { workflows }
     }
 }
 
 pub fn run() {
-    let input = get_input("src/day19/input1.txt");
+    let input = get_input("src/day19/input0.txt");
     let system = System::parse(&input);
-    let ans = system.process_parts();
-    println!("{ans}");
+    // let ans = system.process_parts();
+    // println!("{ans}");
 }
