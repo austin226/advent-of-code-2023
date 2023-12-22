52f908294d37f2ba117bb8c7f4861653c5faa132
Austin Almond <aust.almond@gmail.com>
2023-12-22 00:31:25 -0500

    Track occupied cells

--- src/day22/part1.rs
+++ src/day22/part1.rs
@@ -1,10 +1,12 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 use once_cell::sync::Lazy;
 use regex::Regex;
 
 use crate::common::get_input;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
 struct Point {
     x: i32,
     y: i32,
@@ -17,7 +19,7 @@ impl Point {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Brick {
     ends: (Point, Point),
 }
@@ -36,21 +38,40 @@ impl Brick {
             ),
         }
     }
+
+    fn points(&self) -> Vec<Point> {
+        let (xa, xb) = min_max(self.ends.0.x, self.ends.1.x);
+        let (ya, yb) = min_max(self.ends.0.y, self.ends.1.y);
+        let (za, zb) = min_max(self.ends.0.z, self.ends.1.z);
+
+        let mut p = Vec::new();
+        for x in xa..=xb {
+            for y in ya..=yb {
+                for z in za..=zb {
+                    p.push(Point::new(x, y, z));
+                }
+            }
+        }
+        p
+    }
 }
 
 #[derive(Debug)]
-struct Tower {
+struct Tower<'a> {
     max_x: i32,
     max_y: i32,
     max_z: i32,
-    bricks: Vec<Brick>,
+    cells: HashMap<Point, &'a Brick>,
+    bricks: &'a Vec<Brick>,
 }
 
-impl Tower {
-    fn new(bricks: Vec<Brick>) -> Self {
+impl<'a> Tower<'a> {
+    fn new(bricks: &'a Vec<Brick>) -> Self {
         let mut max_x = 0;
         let mut max_y = 0;
         let mut max_z = 0;
+        let mut cells = HashMap::new();
+
         for brick in bricks.iter() {
             max_x = i32::max(max_x, brick.ends.0.x);
             max_x = i32::max(max_x, brick.ends.1.x);
@@ -58,15 +79,26 @@ impl Tower {
             max_y = i32::max(max_y, brick.ends.1.y);
             max_z = i32::max(max_z, brick.ends.0.z);
             max_z = i32::max(max_z, brick.ends.1.z);
+
+            for point in brick.points() {
+                cells.insert(point, brick);
+            }
         }
 
         Self {
             max_x,
             max_y,
             max_z,
+            cells,
             bricks,
         }
     }
+}
+
+fn min_max(a: i32, b: i32) -> (i32, i32) {
+    let min = i32::min(a, b);
+    let max = i32::max(a, b);
+    (a, b)
 }
 
 pub fn run() {
@@ -75,6 +107,6 @@ pub fn run() {
         .iter()
         .map(|line| Brick::parse(line.as_str()))
         .collect_vec();
-    let tower = Tower::new(bricks);
+    let tower = Tower::new(&bricks);
     println!("{:?}", tower);
 }
