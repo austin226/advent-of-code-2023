c2f99a59e87612784d20c64e5712256916aa8718
Austin Almond <aust.almond@gmail.com>
2023-12-22 00:21:04 -0500

    build tower

--- src/day22/part1.rs
+++ src/day22/part1.rs
@@ -1,7 +1,8 @@
-use crate::common::get_input;
 use itertools::Itertools;
 use once_cell::sync::Lazy;
 use regex::Regex;
+
+use crate::common::get_input;
 
 #[derive(Debug)]
 struct Point {
@@ -37,11 +38,43 @@ impl Brick {
     }
 }
 
+#[derive(Debug)]
+struct Tower {
+    max_x: i32,
+    max_y: i32,
+    max_z: i32,
+    bricks: Vec<Brick>,
+}
+
+impl Tower {
+    fn new(bricks: Vec<Brick>) -> Self {
+        let mut max_x = 0;
+        let mut max_y = 0;
+        let mut max_z = 0;
+        for brick in bricks.iter() {
+            max_x = i32::max(max_x, brick.ends.0.x);
+            max_x = i32::max(max_x, brick.ends.1.x);
+            max_y = i32::max(max_y, brick.ends.0.y);
+            max_y = i32::max(max_y, brick.ends.1.y);
+            max_z = i32::max(max_z, brick.ends.0.z);
+            max_z = i32::max(max_z, brick.ends.1.z);
+        }
+
+        Self {
+            max_x,
+            max_y,
+            max_z,
+            bricks,
+        }
+    }
+}
+
 pub fn run() {
     let input = get_input("src/day22/input0.txt");
     let bricks = input
         .iter()
         .map(|line| Brick::parse(line.as_str()))
         .collect_vec();
-    println!("{:?}", bricks);
+    let tower = Tower::new(bricks);
+    println!("{:?}", tower);
 }
